b08671a30a419631646ffd96f1d53c73e22cab98
12pswathi <Chaithup74@gmail.com>
2025-03-17 17:14:10 +0530

    Updated3

--- Student.java
+++ Student.java
@@ -8,5 +8,6 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
+        System.out.println("Thank you");
         }
 }
